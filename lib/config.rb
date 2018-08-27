50f4775f516a9060600e135cdacf827f1a7c05c9
maratfatkullov <fatk-marat@yandex.ru>
2018-08-27 22:26:05 +0400

    Refactoring. Fix problem with multi node test

--- lib/config.rb
+++ lib/config.rb
@@ -1,34 +1,35 @@
 require "config/version"
+require "pry"
 
 module Config
   class Config
     def initialize(&block)
-      #puts block
+      @hash = {}
       instance_eval &block
     end
 
     def self.build(&block)
-      #puts block
       raise ArgumentError, 'Argument is not given' unless block_given?
       Config.new(&block)
     end
 
     def respond_to_missing?(name)
-      instance_variable_defined?("@#{name}".to_sym)
+      @hash.key?(name.to_sym)
     end
 
     def method_missing(name, *args, &block)
-      # puts 'name'
-      # puts name
-      # puts 'args'
-      # puts args
-      if respond_to_missing?(name)
-        super
-      elsif args[0]
-        instance_variable_set("@#{name}".to_sym, args[0]) if args[0]
-        self.class.send(:define_method, name, proc { instance_variable_get("@#{name}")})
+      if !respond_to_missing?(name) && args[0]
+        @hash[name.to_sym] = args[0]
+        define_singleton_method(name) do |hash = @hash|
+          hash[name.to_sym]
+        end
+      elsif !block.nil?
+        define_singleton_method(name) do |hash = @hash|
+          hash[name.to_sym]
+        end
+        @hash[name.to_sym] = Config.build(&block)
       else
-        Config.build(&block)
+        super
       end
     end
   end
