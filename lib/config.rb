30d0f956ba3b5623053e80034b5f053f03f646dc
maratfatkullov <fatk-marat@yandex.ru>
2018-08-27 16:00:00 +0400

    Build for several node

--- lib/config.rb
+++ lib/config.rb
@@ -3,24 +3,32 @@ require "config/version"
 module Config
   class Config
     def initialize(&block)
+      #puts block
       instance_eval &block
     end
 
     def self.build(&block)
+      #puts block
       raise ArgumentError, 'Argument is not given' unless block_given?
       Config.new(&block)
     end
 
-    def respond_to_missing?(name, *args)
+    def respond_to_missing?(name)
       instance_variable_defined?("@#{name}".to_sym)
     end
 
     def method_missing(name, *args, &block)
-      if respond_to_missing?(name, *args)
+      # puts 'name'
+      # puts name
+      # puts 'args'
+      # puts args
+      if respond_to_missing?(name)
         super
-      else
-        instance_variable_set("@#{name}".to_sym, args[0])
+      elsif args[0]
+        instance_variable_set("@#{name}".to_sym, args[0]) if args[0]
         self.class.send(:define_method, name, proc { instance_variable_get("@#{name}")})
+      else
+        Config.build(&block)
       end
     end
   end
